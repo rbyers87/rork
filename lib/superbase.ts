06feaf3afb18fb039355fc8d35d56c189daea154
rbyers87 <35844687+rbyers87@users.noreply.github.com>
2025-07-21 09:07:39 -0500

    Update superbase.ts

--- lib/superbase.ts
+++ lib/superbase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 import { Database } from '@/types/database';
 
-const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
-const supabaseKey = process.env.EXPO_PUBLIC_SUPABASE_KEY;
+const supabaseUrl = 'https://wkhzlhcjjuzjkmhqtbwd.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndraHpsaGNqanV6amttaHF0YndkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIyMDk4NDUsImV4cCI6MjA2Nzc4NTg0NX0.pJXidfQBrwxUgIX5Qz6xxHg_FM2aYWGcaK7YUCepy74';
 
 if (!supabaseUrl || !supabaseKey) {
   throw new Error('Missing Supabase environment variables. Please check your .env file.');
